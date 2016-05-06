20e386fc6386032eccda71488c409df3812b1dbc
sdkraemer <tfcmaster9@gmail.com>
2016-05-06 16:56:05 -0700

    Populating List of current locations. Listening for PositionChanged event.

---
+++ WhereUAt.Ninja.Mobile/WhereUAt.Ninja.Mobile/Location.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereUAt.Ninja.Mobile
+{
+    public class Location
+    {
+        public double Longitude { get; private set; }
+        public double Latitude { get; private set; }
+
+        
+
+        public Location(double longitude, double latitude)
+        {
+            this.Longitude = longitude;
+            this.Latitude = latitude;
+        }
+
+    }
+}
--- WhereUAt.Ninja.Mobile/WhereUAt.Ninja.Mobile/MainPage.xaml.cs
+++ WhereUAt.Ninja.Mobile/WhereUAt.Ninja.Mobile/MainPage.xaml.cs
@@ -5,15 +5,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
+using System.Threading;
+using AdvancedTimer.Forms.Plugin.Abstractions;
 
 namespace WhereUAt.Ninja.Mobile
 {
     public partial class MainPage : ContentPage
     {
+        private ListView locationListView;
+        private ObservableCollection<string> locationList;
+        private int minTimeInterval = 5000;
+        private double minDistance = 0;
+
         public MainPage()
         {
+            locationList = new ObservableCollection<string>();
+
             Label headerLabel = new Label
             {
                 Text = "Whereu@Ninja",
@@ -28,30 +37,77 @@ namespace WhereUAt.Ninja.Mobile
             };
             button.Clicked += OnGetLocation;
 
+            locationListView = new ListView
+            {
+                ItemsSource = locationList
+            };
+
             this.Content = new StackLayout
             {
                 Children =
                 {
                     headerLabel,
-                    button
+                    button,
+                    locationListView
                 }
             };
-            //InitializeComponent();
+
+            //setupTimer();
+            setupLocationListener();
         }
+
+        private void setupLocationListener()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.StartListeningAsync(minTimeInterval, minDistance, false);
+
+            locator.PositionChanged += (sender, e) =>
+            {
+                var position = e.Position;
+                locationList.Add(String.Format("Longitude: {0} Latitude: {1}", position.Longitude, position.Latitude));
+            };
+        }
+
+
 
         async void OnGetLocation(object sender, EventArgs e)
         {
             try
             {
-                var locator = CrossGeolocator.Current;
-                locator.DesiredAccuracy = 50;
-                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
-                await this.DisplayAlert("Location:", String.Format("Longitude: {0}, Latitude: {1}", position.Longitude, position.Latitude), "OK");
+                //Location location = await getCurrentLocation();
+                //locationList.Add(String.Format("Longitude: {0} Latitude: {1}", location.Longitude, location.Latitude));
             }
             catch (Exception ex)
             {
                 Debug.WriteLine("Unable to get location");
             }
         }
+
+        /*async private Task<Location> getCurrentLocation()
+        {
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+            Location location = new Location(position.Longitude, position.Latitude);
+            return location;
+        }
+
+        private void setupTimer()
+        {
+            IAdvancedTimer timer = DependencyService.Get<IAdvancedTimer>();
+            timer.initTimer(10000, timerElapsed, true);
+            timer.startTimer();
+        }
+
+        async private void addCurrentLocationToList()
+        {
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+            locationList.Add(String.Format("Longitude: {0} Latitude: {1}", position.Longitude, position.Latitude));
+        }
+
+        private void timerElapsed(object sender, EventArgs e)
+        {
+            addCurrentLocationToList();
+        }*/
     }
 }
